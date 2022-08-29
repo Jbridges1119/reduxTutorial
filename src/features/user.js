6ed3d9093ff6fe0690f572a1627bc95357998a24
Jeff <jbridges1119@gmail.com>
2022-08-29 13:32:26 -0600

    Add logout reducer

--- src/features/user.js
+++ src/features/user.js
@@ -1,19 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialStateValue = { name: "", age: 0, email: "" };
+
 export const userSlice = createSlice({
   //Like the state name
-  name: 'user',
+  name: "user",
   //Like useState({name: "", age: 0, email: ''})
-  initialState: {value: {name: "", age: 0, email: ''}},
+  initialState: { value: initialStateValue },
   reducers: {
     //state - holds the current value of the state ~could be initial if we haven't logged in
-    //action - 
-    login: (state, action ) => {
-      state.value = action.payload
+    //action -
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+    logout: (state) => {
+      state.value = initialStateValue;
     },
   },
 });
 
-export const {login} = userSlice.actions
+export const { login, logout } = userSlice.actions;
 
 export default userSlice.reducer;
