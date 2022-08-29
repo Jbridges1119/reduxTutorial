e360f1f9a136425c0623dbea4b9eda7bcec42c3d
Jeff <jbridges1119@gmail.com>
2022-08-29 13:00:39 -0600

    Create reducer and add to global "store"

---
+++ src/features/user.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  //Like the state name
+  name: 'user',
+  //Like useState({name: "", age: 0, email: ''})
+  initialState: {value: {name: "", age: 0, email: ''}},
+  reducers: {
+    //state - holds the current value of the state ~could be initial if we haven't logged in
+    //action - 
+    login: (state, action ) => {
+      state.value = action.payload
+    },
+  },
+});
+
+export default userSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import userReducer from './features/user';
 
+
+//
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    //User state from user.js
+    user: userReducer,
+  }
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
     <App />
     </Provider>
   </React.StrictMode>
