99bfd763666a9340eea14613c0bbc9daf339ee2d
Jeff <jbridges1119@gmail.com>
2022-08-29 13:02:43 -0600

    import global user state and pass into markup

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+//hook to grab values of our state when we need it
+import { useSelector } from 'react-redux'
 
 function Profile() {
+  //Like a deconstructed Prop ~state.user.value from our user.js
+  const user = useSelector((state) => state.user.value)
   return (
     <div>
       <h1>Profile Page</h1>
-      <p>Name:</p>
-      <p>Email:</p>
-      <p>Age:</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Age: {user.age}</p>
     </div>
   )
 }
